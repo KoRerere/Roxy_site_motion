2c8153598e1ba1a7fd27d8ad6440936057d7cfaa
Anbin <z_anbin@163.com>
2025-07-25 10:54:12 +0800

    移除不必要的项并添加新项，同时在 useScripts 中添加 Clarity 脚本支持。

--- layouts/hooks/useScripts.ts
+++ layouts/hooks/useScripts.ts
@@ -64,7 +64,14 @@ export const useScripts = () => {
       'data-key': 'pWhhdvWZr/fhpDi51co32A',
       async: true
     })
-  } 
+  } else {
+    scripts.push({
+      type: 'text/javascript',
+      textContent: `
+        (function(c,l,a,r,i,t,y){ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); })(window, document, "clarity", "script", "sk7g3nawyf");
+      `
+    })
+  }
 
   return scripts
 }
